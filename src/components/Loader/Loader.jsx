76f45f7174cfe00db74bb2c296c7dc719567ddde
CherniukAnna <chernukanna@gmail.com>
2023-03-11 09:48:52 +0200

    Fixed modal

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,8 +5,8 @@ export const Loader = () => {
   return (
     <div className="Loader">
       <RotatingLines
-        strokeColor="#3f51b590"
-        strokeWidth="5"
+        strokeColor="rgba(255, 192, 203, 0.824)"
+        strokeWidth="1"
         animationDuration="0.75"
         width="96"
         visible={true}
